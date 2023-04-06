67fec36544e99a82b926d4aa223baaa47fa46f0a
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-04-06 10:50:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import axios from "axios";
 import { Notify } from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const API_KEY = '34720364-25bd7ca6aede9db569324b321';
+const BASE_URL = 'https://pixabay.com/api/';
